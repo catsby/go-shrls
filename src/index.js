b1f370403a5e658b7c815a271edbd4aef2da28e2
catsby <clint@ctshryock.com>
2022-04-29 09:02:40 -0500

    remove hello

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ const app = new Vue({
 
         <div class="container mb-6 mt-4">
 <!-- add some content here -->
-Hello, Waypoint demo!
+<!-- end -->
 
             <div class="columns is-centered">
                 <div class="column is-two-thirds">
