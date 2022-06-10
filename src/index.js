688ee866b5f964de781fffc2e25be24a9f943c58
catsby <clint@ctshryock.com>
2022-06-10 17:20:00 -0500

    update to version 2

--- src/index.js
+++ src/index.js
@@ -58,7 +58,7 @@ const app = new Vue({
 
         <div class="container mb-6 mt-4">
 <!-- add some content here -->
-Hello HashiConf Europe Demo V1
+Hello HashiConf Europe Demo V2
 <!-- end -->
 
             <div class="columns is-centered">
